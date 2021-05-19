c437e2c673f28be5d81f5f0bdc5da3b9352254ff
Aashima wadhwa <73706697+aashimawadhwa@users.noreply.github.com>
2021-05-19 22:49:31 +0530

    Create reverse _a_stack.cpp

---
+++ reverse _a_stack.cpp
@@ -0,0 +1,20 @@
+void reverseStack(stack<int> &input, stack<int> &extra) {
+    if(input.size()==0 || input.size()==1)
+    {
+      return;
+    }
+    int x=input.top();
+    input.pop();
+    reverseStack(input,extra);
+    while(!input.empty())
+    {
+      extra.push(input.top());
+      input.pop();
+    }
+    input.push(x);
+    while(!extra.empty())
+    {
+      input.push(extra.top());
+      extra.pop();
+    }
+}
